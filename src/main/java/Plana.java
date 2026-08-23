30f1bc8182b1da74a6808917a8a40ad5ec30a57c
neokh719 <neokiahao@gmail.com>
2026-08-23 23:39:50 +0800

    Add ability to store whatever text the user enters and display it back when requested

--- src/main/java/Plana.java
+++ src/main/java/Plana.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 
 public class Plana {
+    private static final int MAX_TASKS = 100;
+
     public static void main(String[] args) {
         String banner = " ____  _                  \n"
                 + "|  _ \\| | __ _ _ __   __ _ \n"
@@ -15,6 +17,9 @@ public class Plana {
         System.out.println(border_line);
 
         try (Scanner scanner = new Scanner(System.in)) {
+            String[] tasks = new String[MAX_TASKS];
+            int taskCount = 0;
+
             while (scanner.hasNextLine()) {
                 String command = scanner.nextLine();
                 System.out.println(command);
@@ -24,6 +29,20 @@ public class Plana {
                     System.out.println("Bye. Hope to see you again soon!");
                     System.out.println(border_line);
                     break;
+                } else if (command.equals("list")) {
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                    }
+                    System.out.println(border_line);
+                } else if (!command.isBlank()) {
+                    if (taskCount < MAX_TASKS) {
+                        tasks[taskCount] = command;
+                        taskCount++;
+                        System.out.println("added: " + command);
+                    } else {
+                        System.out.println("Sorry, your task list is full.");
+                    }
+                    System.out.println(border_line);
                 }
             }
         }
