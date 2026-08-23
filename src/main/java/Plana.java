3a19ab7227172eec69b17f5204c1c36600433b76
neokh719 <neokiahao@gmail.com>
2026-08-23 23:33:47 +0800

    Update code to echo commands entered by user and exit when user types command bye

--- src/main/java/Plana.java
+++ src/main/java/Plana.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Plana {
     public static void main(String[] args) {
         String banner = " ____  _                  \n"
@@ -11,7 +13,19 @@ public class Plana {
         System.out.println("Hello! I'm Plana.");
         System.out.println("What can I do for you?");
         System.out.println(border_line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(border_line);
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (scanner.hasNextLine()) {
+                String command = scanner.nextLine();
+                System.out.println(command);
+                System.out.println(border_line);
+
+                if (command.equals("bye")) {
+                    System.out.println("Bye. Hope to see you again soon!");
+                    System.out.println(border_line);
+                    break;
+                }
+            }
+        }
     }
 }
